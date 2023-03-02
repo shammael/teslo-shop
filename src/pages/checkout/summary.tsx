fe2af0a043c14cb3c846bc7f196f611eb736f38b
Shammael <shammamnd2015@gmail.com>
2023-03-02 08:28:40 -0400

    fix: :bug: Fix cart summary page
    * Add products to to cart list via context and pass total params to checout summary component

--- src/pages/checkout/summary.tsx
+++ src/pages/checkout/summary.tsx
@@ -4,6 +4,7 @@ import {
   OrdenSummary,
   ShopLayout,
 } from '@/components';
+import { CartContext } from '@/context';
 import {
   Card,
   Grid,
@@ -13,47 +14,50 @@ import {
   Box,
   Button,
 } from '@mui/material';
-import { ReactElement } from 'react';
+import { ReactElement, useContext } from 'react';
 import { NextPageWithLayout } from '../_app';
 
-const SummaryPage: NextPageWithLayout = () => (
-  <Box sx={{ maxWidth: '1200px', mx: 'auto' }}>
-    <Typography variant="h1" component="h1">
-      Resumen de la orden
-    </Typography>
-    <Grid
-      container
-      marginTop={2}
-      sx={{
-        position: 'relative',
-        justifyContent: { xs: 'center', md: 'space-between' },
-      }}
-    >
-      <Grid item xs={12} md={7}>
-        <CartList />
-      </Grid>
-      <Grid item xs={12} md={4} style={{ maxWidth: '400px' }}>
-        <Card className="summary-card">
-          <CardContent>
-            <Typography variant="h2" component="h2">
-              Resumen
-            </Typography>
-            <Divider sx={{ my: 1 }} />
-            <DirectionSummary />
-            <Divider sx={{ my: 1 }} />
-            <OrdenSummary />
+const SummaryPage: NextPageWithLayout = () => {
+  const { cart } = useContext(CartContext);
+  return (
+    <Box sx={{ maxWidth: '1200px', mx: 'auto' }}>
+      <Typography variant="h1" component="h1">
+        Resumen de la orden
+      </Typography>
+      <Grid
+        container
+        marginTop={2}
+        sx={{
+          position: 'relative',
+          justifyContent: { xs: 'center', md: 'space-between' },
+        }}
+      >
+        <Grid item xs={12} md={7}>
+          <CartList products={cart.products} />
+        </Grid>
+        <Grid item xs={12} md={4} style={{ maxWidth: '400px' }}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2" component="h2">
+                Resumen
+              </Typography>
+              <Divider sx={{ my: 1 }} />
+              <DirectionSummary />
+              <Divider sx={{ my: 1 }} />
+              <OrdenSummary total={cart.total} />
 
-            <Box sx={{ mt: 3 }}>
-              <Button color="secondary" className="circular-btn" fullWidth>
-                Confirmar orden
-              </Button>
-            </Box>
-          </CardContent>
-        </Card>
+              <Box sx={{ mt: 3 }}>
+                <Button color="secondary" className="circular-btn" fullWidth>
+                  Confirmar orden
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
-  </Box>
-);
+    </Box>
+  );
+};
 
 SummaryPage.getLayout = (page: ReactElement) => (
   <ShopLayout title="Summary" pageDescription="Resumen de la orden">
